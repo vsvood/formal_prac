7e2070b9b2c80b901a505faed727e3469b05bda4
Vsevolod Kaspirovich <kaspirovich.vm@phystech.edu>
2022-11-16 17:54:53 +0300

    CYK with tests

---
+++ prac_2/cf_grammar/__init__.py
@@ -0,0 +1,4 @@
+from .cf_grammar import CFGrammar
+from .mutator import convert_to_chomsky
+
+__all__ = ["CFGrammar", "convert_to_chomsky"]
--- prac_2/main.py
+++ prac_2/main.py
@@ -1,7 +1,15 @@
-import mutator
-from cf_grammar import CFGrammar
+from cf_grammar import CFGrammar, mutator
+from cyk_algo import cyk_check
 
-with open("tests/mutators/CBS") as f:
+with open("tests/unified_test_in") as f:
     grammar = CFGrammar(f.read())
 grammar = mutator.convert_to_chomsky(grammar)
 print(grammar)
+
+with open("tests/test") as f:
+    count = 0
+    for line in f.readlines():
+        count += 1
+        print(count)
+        phrase, truth = line.split()
+        assert cyk_check(grammar, phrase) != truth
