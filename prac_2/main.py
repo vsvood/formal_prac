aa8918a1deea328210676cbeba6e8772bc1f8924
Vsevolod Kaspirovich <kaspirovich.vm@phystech.edu>
2022-11-16 19:39:36 +0300

    main and docs

--- prac_2/main.py
+++ prac_2/main.py
@@ -1,15 +1,17 @@
+import argparse
+
 from cf_grammar import CFGrammar, mutator
 from cyk_algo import cyk_check
 
-with open("tests/unified_test_in") as f:
-    grammar = CFGrammar(f.read())
-grammar = mutator.convert_to_chomsky(grammar)
-print(grammar)
-
-with open("tests/test") as f:
-    count = 0
-    for line in f.readlines():
-        count += 1
-        print(count)
-        phrase, truth = line.split()
-        assert cyk_check(grammar, phrase) != truth
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        prog='cyk_check',
+        description='Check if a phrase is in a language defined grammar',
+        epilog='(c) 2022, @vsvood')
+    parser.add_argument('grammar', type=str, help='Path to grammar file')
+    parser.add_argument('phrase', type=str, help='Phrase to check')
+    args = parser.parse_args()
+    with open(args.grammar) as f:
+        grammar = CFGrammar(f.read())
+    grammar = mutator.convert_to_chomsky(grammar)
+    print(cyk_check(grammar, args.phrase))
